2f61882ded6e74b20d53ec57dbf342f39ea6b1de
Matthew Planchard <msplanchard@gmail.com>
2017-06-11 12:31:16 -0500

    autodoc fixes

--- docs/conf.py
+++ docs/conf.py
@@ -53,6 +53,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
+    # 'sphinx_autodoc_typehints',  # uncomment to support type-hinting
 ]
 
 # Add any paths that contain templates here, relative to this directory.
